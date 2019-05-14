0b6fb354e510e89786913979e1db1edad6687338
Oscar Goh <oscar.goh@gmail.com>
2019-05-13 22:44:50 -0500

    Create the genesis block

--- blockchain.py
+++ blockchain.py
@@ -9,6 +9,9 @@ class Blockchain(object):
         self.chain = []
         self.current_transactions = []
 
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
+
     def new_block(self, proof, previous_hash=None):
         """
         Create a new Block in the Blockchain
